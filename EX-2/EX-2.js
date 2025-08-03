53aa77b529f48e0804d97488d86fd6f86d620ca9
PromSereyreaka <prumereyreaka@gmail.com>
2025-08-03 10:25:45 -0400

    Week 01 Lab and Homework

---
+++ EX-2/EX-2.js
@@ -0,0 +1,10 @@
+// ===== LAB EXERCISE 2 STARTER TEMPLATE =====
+// Arithmetic Operators
+
+console.log("=== Lab 2: Arithmetic Operators ===");
+
+// TODO: Step 1 - Declare two numbers
+
+// TODO: Step 2 - Perform and store sum, difference, product, and quotient
+
+// TODO: Step 3 - Display all four results using console.log
